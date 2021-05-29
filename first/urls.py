e0ce7dcc4bf6fed2598b9f0bc0fb0b8329df4b17
Gajendar <gyrao72@gmail.com>
2021-05-29 23:31:17 +0530

    complete css

--- first/urls.py
+++ first/urls.py
@@ -6,12 +6,13 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
-    path('',views.home),
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
-    path('accounts/',include('accounts.urls'))
+    path('accounts/',include('accounts.urls')),
+    path('',article_views.articles_list,name="home"),
 ]
 
 urlpatterns+=staticfiles_urlpatterns()
